61bc5f09009bf5f756e4ba655c9794928d1d1a04
Allen <30354266+alllenski@users.noreply.github.com>
2022-04-24 02:27:39 +0800

    Build upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1650738330,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/player-sheet0.png",
+		"images/button-sheet0.png",
+		"images/checkpoint-sheet0.png",
+		"images/exit-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/winparticles.png",
+		"images/movingblock-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
